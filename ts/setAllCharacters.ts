61b696bc77331bb5e5b0b45867e587578a6aa269
Yurii Kovalchuk <yura.kovalchuk91@gmail.com>
2022-09-03 10:39:25 +0300

    v2

---
+++ ts/setAllCharacters.ts
@@ -0,0 +1,44 @@
+import CharacterInterface from "./CharacterInterface";
+
+const generateHtmlTemplate = (char: CharacterInterface): string => {
+  const { id, name, status, image, species, type, gender, location } = char;
+  const isDead: boolean = status === 'Dead' ? true : false;
+  return `
+  <div class="character ${isDead ? 'dead' : ''}" data-id="${id}">
+    <div class="character__main-info">
+      <img class="character__img" src="${image}" alt="eweq">
+      <p class="character__status ${isDead ? 'dead-status' : 'alive-status'}">${status}</p>
+      <p class="character__name">${name}</p>
+    </div>
+    <div class="character__info">
+      <p class="character__species">Species: ${species}</p>
+      <p class="character__type">Type: ${type ? type : 'Unknown'}</p>
+      <p class="character__gender">Gender: ${gender}</p>
+      <p class="character__location">Location: ${location.name}</p>
+    </div>
+    <button class="character__delete-btn">&#9587</button>
+  </div>
+  `
+}
+
+const setAllCharacters = (characters: CharacterInterface[], COUNTOFCHAR: number): number => {
+  const loadMoreBtn = document.querySelector('.more-btn') as HTMLButtonElement;
+  const charWrap = document.querySelector('.character-wrap') as HTMLDivElement;
+  const count: number = COUNTOFCHAR < characters.length ? COUNTOFCHAR : characters.length;
+  charWrap.innerHTML = '';
+  for (let i: number = 0; i < count; i++) {
+    const char = characters[i];
+    charWrap.innerHTML += generateHtmlTemplate(char)
+  }
+  if (characters.length > COUNTOFCHAR) {
+    loadMoreBtn.classList.remove('hidden');
+  } else {
+    loadMoreBtn.classList.add('hidden');
+  }
+  if (characters.length % 5 === 0 && loadMoreBtn.classList.contains('hidden')) {
+    return COUNTOFCHAR = characters.length;
+  }
+  return COUNTOFCHAR
+}
+
+export default setAllCharacters
